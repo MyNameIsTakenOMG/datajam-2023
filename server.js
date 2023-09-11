b990bccfaf07f7e980eedbfc762c7b58c4a31c2c
Sean Fang <fangzhengonly@gmail.com>
2023-09-11 10:48:25 -0400

    add eventTransform method to transform event objects

--- server.js
+++ server.js
@@ -48,15 +48,8 @@ wss.on('connection', async function connection(ws, req) {
     await eventsFile.read((line) => {
       // send out the event of the game
       if (pointer === gameNumber) {
-        const dto = {
-          type: line.events[0].type,
-          actor: line.events[0].actor,
-          action: line.events[0].action,
-          target: line.events[0].target,
-        };
-        ws.send(JSON.stringify(dto));
-        if (line.events[0].type === 'player-acquired-item') {
-        }
+        let message = eventTransform(line);
+        ws.send(JSON.stringify(message));
 
         // reach the end of the game
         if (line.events[0].type === 'team-won-game') {
@@ -83,3 +76,179 @@ wss.on('connection', async function connection(ws, req) {
 
   // eventsFile.read((line) => ws.send(JSON.stringify(line)));
 });
+
+const eventTransform = (line) => {
+  let message = {};
+  // if it's a ban/pick event
+  if (line.events[0].type.match(/(banned|picked)/)) {
+    message = {
+      log: `${line.events[0].actor.state.series.name} ${line.events[0].action} ${line.events[0].target.id}`,
+      draftAction: {
+        sequenceNumber:
+          line.events[0].seriesStateDelta.games[0].draftActions[0]
+            .sequenceNumber,
+        type: line.events[0].action,
+        drafter: {
+          id: line.events[0].actor.id,
+          type: line.events[0].actor.type,
+          name: line.events[0].actor.state.series.name,
+        },
+        draftable: {
+          id: line.events[0].target.id,
+          type: line.events[0].target.type,
+        },
+      },
+    };
+  }
+  // if it's an acquired/equipped/unequipped/stashed/unstashed event
+  else if (
+    line.events[0].type.match(/acquired|equipped|unequipped|stashed|unstashed/)
+  ) {
+    message = {
+      log: `${line.events[0].actor.state.series.name} ${
+        line.events[0].action
+      } ${line.events[0].target.id.replaceAll('_', ' ')}`,
+      itemAction: {
+        id: line.events[0].actor.id,
+        teamId: line.events[0].actor.state.teamId,
+        type: line.events[0].action,
+        item: {
+          id: line.events[0].target.state.id,
+          equipped: line.events[0].target.state.equipped,
+          stashed: line.events[0].target.state.stashed,
+        },
+      },
+    };
+  }
+  // if it's a lost-item event
+  else if (line.events[0].type.match(/lost-item/)) {
+    message = {
+      log: `${line.events[0].actor.state.series.name} ${
+        line.events[0].action
+      } ${line.events[0].target.id.replaceAll('_', ' ')}`,
+      itemAction: {
+        id: line.events[0].actor.id,
+        teamId: line.events[0].actor.state.teamId,
+        type: line.events[0].action,
+        item: {
+          id: line.events[0].target.state.id,
+        },
+      },
+    };
+  }
+  // if it's a game-set-clock event
+  else if (line.events[0].type.match(/game-set-clock/)) {
+    const teams = line.events[0].actor.state.teams.map((team) => {
+      return {
+        id: team.id,
+        name: team.name,
+        side: team.side,
+        won: team.won,
+        players: team.players.map((player) => {
+          return {
+            id: player.id,
+            name: player.name,
+            character: player.id,
+          };
+        }),
+      };
+    });
+
+    message = {
+      log: `${line.events[0].type.replaceAll('_', ' ')}`,
+      teams: teams,
+    };
+  }
+  // if it's a increaseLevel event
+  else if (line.events[0].type.match(/increaseLevel/)) {
+    message = {
+      log: `${line.events[0].actor.state.name} leveled up`,
+      player: {
+        teamId: line.events[0].actor.state.teamId,
+        id: line.events[0].actor.state.id,
+        objective: {
+          completionCount:
+            line.events[0].actor.state.series.objectives[0].completionCount,
+          type: line.events[0].actor.state.series.objectives[0].type,
+        },
+      },
+    };
+  }
+  // if it's a player-killed-player event
+  else if (line.events[0].type.match(/player-killed-player/)) {
+    message = {
+      log: `${line.events[0].actor.state.series.name} ${line.events[0].action} ${line.events[0].target.state.series.name}`,
+      player: {
+        actor: {
+          id: line.events[0].actor.id,
+          assists:
+            line.events[0].actor.stateDelta.game.killAssistsReceivedFromPlayer,
+          teamId: line.events[0].actor.state.teamId,
+        },
+        target: {
+          id: line.events[0].target.id,
+          teamId: line.events[0].target.state.teamId,
+        },
+      },
+    };
+  }
+  // if it's a team-killed-player event
+  else if (line.events[0].type.match(/team-killed-player/)) {
+    message = {
+      log: `${line.events[0].actor.state.series.name} ${line.events[0].action} ${line.events[0].target.state.series.name}`,
+      teamId: line.events[0].actor.id,
+      target: {
+        id: line.events[0].target.id,
+        teamId: line.events[0].target.state.teamId,
+      },
+    };
+  }
+
+  // if it's a grid, tournament, series-related event
+  else if (line.events[0].type.match(/grid|tournament|series/)) {
+    message = {
+      log: `${line.events[0].type.replaceAll('-', ' ')}`,
+    };
+  }
+
+  // for rest of the player-related events
+  else if (line.events[0].type.match(/player-/)) {
+    let log = '';
+    if (line.events[0].type.match(/player-selfrevived-player/))
+      log = line.events[0].actor.state.series.name + ' self-revived';
+    else
+      log = line.events[0].type
+        .replace(/player/, line.events[0].actor.state.series.name)
+        .replaceAll('-', ' ');
+    message = {
+      log: log,
+    };
+  }
+
+  // for rest of the team-related events
+  else if (line.events[0].type.match(/team-/)) {
+    message = {
+      log: line.events[0].type
+        .replace(/team/, line.events[0].actor.state.series.name)
+        .replaceAll('-', ' '),
+    };
+  }
+
+  // for rest of the game-related events
+  else if (line.events[0].type.match(/game-/)) {
+    let log = '';
+    if (line.events[0].type.endsWith('roshan')) {
+      log = 'Roshan respawned';
+    } else if (line.events[0].type.endsWith('player')) {
+      log = `${line.events[0].target.state.series.name} respawned`;
+    } else {
+      log = 'Roshan respawn clock set';
+    }
+    message = {
+      log,
+    };
+  }
+
+  message.eventType = line.events[0].type;
+  return message;
+};
